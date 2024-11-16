2f6f2c0ccbf401747efcf1de6d345206da9c9d88
Emre Çanakçı <emreosmancanakci@gmail.com>
2024-11-16 09:07:21 +0300

    Login tesleri yapildi. Hatala ve Basarili giris test edildi

---
+++ LoginTest.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from urllib3.packages.six import BytesIO
+
+service = Service("./chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+driver.get("https://the-internet.herokuapp.com/login")
+
+username = driver.find_element(By.ID, "username")
+username.send_keys("emreosman")
+
+password = driver.find_element(By.ID, "password")
+password.send_keys("123321")
+
+loginButton = driver.find_element(By.CLASS_NAME, "radius").click()
+
+result = driver.find_element(By.ID,"flash-messages").text
+
+if "Your username is invalid!" in result:
+    print("Kullanici adi hatali...")
+else:
+    print("Kullanici adi doğru...")
+
+
+driver.get("https://the-internet.herokuapp.com/login")
+
+driver.find_element(By.ID, "username").send_keys("tomsmith")
+driver.find_element(By.ID, "password").send_keys("123321")
+driver.find_element(By.CLASS_NAME, "radius").click()
+result = driver.find_element(By.ID, "flash-messages").text
+
+if "Your password is invalid!" in result:
+    print("Sifre hatali...")
+else:
+    print("Sifre dogru")
+
+driver.find_element(By.ID, "username").send_keys("tomsmith")
+driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+driver.find_element(By.CLASS_NAME, "radius").click()
+result = driver.find_element(By.ID, "flash-messages").text
+
+if "You logged into a secure area!" in result:
+    print("Tebrikler Basarili Giris...")
+else:
+    print("Tekrar deneyiniz...")
+
+
+
+driver.quit()
