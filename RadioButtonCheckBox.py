7c5a33dee6b15a9e438e32ffcc1354b95e73c617
Emre Çanakçı <emreosmancanakci@gmail.com>
2024-11-18 21:49:26 +0300

    Static Dropdown

--- RadioButtonCheckBox.py
+++ RadioButtonCheckBox.py
@@ -1,6 +1,10 @@
+import time
+
+from select import select
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 service = Service("./chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -16,6 +20,23 @@ middle.click()
 zeytin.click()
 print(middle.is_selected())
 print(zeytin.is_selected())
+
+print("******************************")
+
+dropDown = driver.find_element(By.ID, "odeme-tipi")
+odeme = Select(dropDown)
+odeme_tipleri = odeme.options
+
+for tip in odeme_tipleri:
+    print(tip.text)
+
+time.sleep(2)
+odeme.select_by_visible_text("Kredi Kartı")
+time.sleep(2)
+odeme.select_by_index(2)
+time.sleep(2)
+driver.quit()
+
 
 
 
